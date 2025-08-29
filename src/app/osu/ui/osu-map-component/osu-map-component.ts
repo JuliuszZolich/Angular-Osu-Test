71e315694070bd675e9760bcbdc9882f5056180f
Juliusz Zolich <juliusz.zolich@kernelpanic.com.pl>
2025-08-29 16:31:03 +0200

    better styling and content

--- src/app/osu/ui/osu-map-component/osu-map-component.ts
+++ src/app/osu/ui/osu-map-component/osu-map-component.ts
@@ -1,21 +1,29 @@
 import { Component, effect, inject, Input, input, HostListener } from '@angular/core';
 import { OsuMapStateService } from './osu-map-state-service';
+import { convertEnumToGenre, convertEnumToLanguage } from '../../utils/enum-converter';
+import { GenresEnum } from '../../model/genres';
+import { LanguagesEnum } from '../../model/languages';
 
 @Component({
-  selector: 'app-osu-map-component',
+  selector: 'osu-map-component',
   imports: [],
   templateUrl: './osu-map-component.html',
-  styleUrl: './osu-map-component.css',
+  styleUrl: './osu-map-component.scss',
 })
 export class OsuMapComponent {
   protected beatmapState = inject(OsuMapStateService);
   protected isVisible = false;
 
-  constructor(){
-     effect(() => {
+  protected genre = '';
+  protected language = '';
+
+  constructor() {
+    effect(() => {
       //Efekt do odczytu zmian w sygnale
       const beatmap = this.beatmapState.currentBeatmap();
       if (beatmap) {
+        this.genre = convertEnumToGenre(beatmap.beatmapset?.genre_id as GenresEnum);
+        this.language = convertEnumToLanguage(beatmap.beatmapset?.language_id as LanguagesEnum)
         this.isVisible = true;
       }
     });
