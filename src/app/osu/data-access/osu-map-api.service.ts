116e521b09b61f280fb790212c5a0105f2269912
Juliusz Zolich <juliusz.zolich@kernelpanic.com.pl>
2025-09-02 11:19:12 +0200

    map caching

--- src/app/osu/data-access/osu-map-api.service.ts
+++ src/app/osu/data-access/osu-map-api.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { forkJoin, Observable, switchMap } from 'rxjs';
+import { forkJoin, Observable, switchMap, tap } from 'rxjs';
 import { map } from 'rxjs';
 import { Score } from '../model/score';
 import { OsuHttpClient } from './osu-httpclient.service';
@@ -12,34 +12,25 @@ import { baseUrl } from './shared/access-urls';
 export class OsuApiMapService {
   private httpClient = inject(OsuHttpClient);
 
-  getMapsForScores(scores: Score[]): Observable<Score[]> {
-    const chunks: Score[][] = [];
-    //Api przyjmuje albo pojedyncze albo w chunkach do max 50
-    //Aby zmniejszyć zużycie robiłem w chunkach
-    for (let i = 0; i < scores.length; i += 50) {
-      chunks.push(scores.slice(i, i + 50));
-    }
+  private beatmapCache = new Map<number, Beatmap>();
 
-    const requests = chunks.map((chunk) =>
-      this.httpClient
-        .get<{ beatmaps: Beatmap[] }>(baseUrl + '/beatmaps?' + this.parseIds(chunk))
-        .pipe(
-          map((res) => res.beatmaps),
-          map((maps) => {
-            const beatmapMap = new Map(maps.map(beatmap => [beatmap.id, beatmap]));
-            
-            return chunk
-              //Api czasami nie zwraca wszystkich map dlatego są filtrowane te z których nie ma danych
-              .filter(score => beatmapMap.has(score.beatmap_id))
-              .map(score => ({
-                ...score,
-                beatmap: beatmapMap.get(score.beatmap_id)!
-              }));
-          })
-        )
+  getMapsForScores(scores: Score[]): Observable<Score[]> {
+    console.log(this.beatmapCache)
+    var scoresWithNewBeatMap = scores.filter(
+      (score) => this.beatmapCache.get(score.beatmap_id) == undefined
+    );
+    var scoresWithoutNewBeatMap = scores.filter(
+      (score) => this.beatmapCache.get(score.beatmap_id) != undefined
     );
 
-    return forkJoin(requests).pipe(map((results) => results.flat()));
+    console.log("Cached maps: " + scoresWithoutNewBeatMap.length)
+    console.log("Not cached maps: " + scoresWithNewBeatMap.length)
+    const requests = this.getNewMaps(scoresWithNewBeatMap);
+    const restOfScores = this.getMapsFromCache(scoresWithoutNewBeatMap);
+    return forkJoin(requests).pipe(
+      map((results) => [...(results.flat() as Score[]), ...restOfScores]),
+      map((results) => results.sort((s1,s2)=>Date.parse(s2.ended_at)-Date.parse(s1.ended_at)))
+    );
   }
 
   private parseIds(scores: Score[]) {
@@ -49,4 +40,43 @@ export class OsuApiMapService {
     }
     return value;
   }
+
+  private getNewMaps(scoresWithNewBeatMap: Score[]) {
+    const chunks: Score[][] = [];
+
+    //Api przyjmuje albo pojedyncze albo w chunkach do max 50
+    //Aby zmniejszyć zużycie robiłem w chunkach
+    for (let i = 0; i < scoresWithNewBeatMap.length; i += 50) {
+      chunks.push(scoresWithNewBeatMap.slice(i, i + 50));
+    }
+
+    return chunks.map((chunk) =>
+      this.httpClient
+        .get<{ beatmaps: Beatmap[] }>(baseUrl + '/beatmaps?' + this.parseIds(chunk))
+        .pipe(
+          map((res) => res.beatmaps),
+          map((maps) => {
+            const beatmapMap = new Map(maps.map((beatmap) => [beatmap.id, beatmap]));
+
+            return (
+              chunk
+                //Api czasami nie zwraca wszystkich map dlatego są filtrowane te z których nie ma danych
+                .filter((score) => beatmapMap.has(score.beatmap_id))
+                .map((score) => ({
+                  ...score,
+                  beatmap: beatmapMap.get(score.beatmap_id)!,
+                }))
+            );
+          }),
+          tap((scores)=>scores.forEach((score)=>this.beatmapCache.set(score.beatmap_id, score.beatmap)))
+        )
+    );
+  }
+
+  private getMapsFromCache(scoresWithoutNewBeatMap: Score[]) {
+    scoresWithoutNewBeatMap.forEach((score)=>{
+      score.beatmap = this.beatmapCache.get(score.beatmap_id);
+    })
+    return scoresWithoutNewBeatMap;
+  };
 }
